e3ce3119815d662c32b2f19679035f8f32301d08
Brian Dys Sahagun <briansahagun@gmail.com>
2017-05-08 19:06:52 +0800

    Updates

--- inc/functions/markup.php
+++ inc/functions/markup.php
@@ -1,20 +1,59 @@
 <?php
+/*
+// Component
+$GLOBALS['c_mu'] = '<div class="c p %2$s" data-name="%1$s"><div class="h"><span class="l">%1$s</span></div><div class="ct">%3$s</div></div><!-- %1$s -->';
 
-$GLOBALS['cp_mu'] = '<div class="cp %2$s" data-name="%1$s">';
-    $GLOBALS['cp_mu'] .= '<div class="cr %3$s---cr">';
-        $GLOBALS['cp_mu'] .= '<div class="h %3$s---h"><span class="h_l %3$s---h_l">%1$s</span></div>';
-        $GLOBALS['cp_mu'] .= '<div class="ct %3$s---ct">';
-            $GLOBALS['cp_mu'] .= '<div class="ct_cr %3$s---ct_cr">';
-                $GLOBALS['cp_mu'] .= '%4$s';
-            $GLOBALS['cp_mu'] .= '</div>';
-        $GLOBALS['cp_mu'] .= '</div><!-- ct -->';
-    $GLOBALS['cp_mu'] .= '</div>';
-$GLOBALS['cp_mu'] .= '</div><!-- %1$s -->';
+// Object
+$GLOBALS['o_mu'] = '<div class="o p %2$s-obj" data-name="%1$s">%3$s</div><!-- %1$s -->';
 
-$GLOBALS['obj_mu'] = '<span class="obj %2$s" data-name="%1$s">';
-    $GLOBALS['obj_mu'] .= '<span class="g %3$s---g">';
-        $GLOBALS['obj_mu'] .= '<span class="g_l %3$s---g_l">';
-            $GLOBALS['obj_mu'] .= '%4$s';
-        $GLOBALS['obj_mu'] .= '</span>';
-    $GLOBALS['obj_mu'] .= '</span>';
-$GLOBALS['obj_mu'] .= '</span><!-- %1$s -->';
+// Frame
+$GLOBALS['f_mu'] = '<div class="f p %1$s-f">%2$s</div>';
+
+
+// Text
+$GLOBALS['t_mu'] = '<span class="t">%1$s</span>';
+
+
+// Sample Constuctor        
+$wbp_title_obj_a = sprintf( '<a class="a" href="%2$s">' . $GLOBALS['t_mu'] . '</a>',
+    'The Title',
+    '#url'
+);
+
+$wbp_title_obj_h = sprintf( '<h1 class="h">%1$s</h1>',
+    $wbp_title_obj_a
+);
+
+$wbp_title_obj_NAME = 'Web Product Title';
+$wbp_title_obj = sprintf( $GLOBALS['o_mu'],
+    $wbp_title_obj_NAME,
+    'wbp-title',
+    $wbp_title_obj_h
+);
+
+// Web Product Description
+// Hello, this is the description of WordPress.
+
+$wbp_desc_obj_a = sprintf( '<a class="a" href="%2$s">' . $GLOBALS['t_mu'] . '</a>',
+    'Hello, this is the description of WordPress.',
+    '#url'
+);
+
+$wbp_desc_obj_h = sprintf( '<span class="g">%1$s</span>',
+    $wbp_desc_obj_a
+);
+
+$wbp_desc_obj_NAME = 'Web Product Description';
+$wbp_desc_obj = sprintf( $GLOBALS['o_mu'],
+    $wbp_desc_obj_NAME,
+    'wbp-desc',
+    $wbp_desc_obj_h
+);
+
+$wbp_title_desc_f = sprintf( $GLOBALS['f_mu'],
+    'wbp-title-desc',
+    $wbp_title_obj . $wbp_desc_obj
+);
+
+printf( $wbp_title_desc_f );
+*/
--- snapons/applicator/index.php
+++ snapons/applicator/index.php
@@ -53,7 +53,7 @@ if ( ! function_exists( 'apl_snapons_applicator_func_styles' ) ) {
         wp_enqueue_style( 'apl-style-fonts', applicator_fonts_url(), array(), null );
         add_editor_style( array( 'assets/css/editor-style.css', applicator_fonts_url() ) );
         
-        wp_enqueue_style( 'apl-snapons-applicator-style', get_theme_file_uri() . '/snapons/applicator/assets/applicator.css', array(), '17.2', 'all' );
+        wp_enqueue_style( 'apl-snapons-applicator-style', get_theme_file_uri() . '/snapons/applicator/assets/applicator.css', array(), '17.3', 'all' );
 
     }
     add_action( 'wp_enqueue_scripts', 'apl_snapons_applicator_func_styles', 0);
