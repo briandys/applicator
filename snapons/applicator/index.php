68557f6b0b16de9a3899be0236216adae5d2de47
Brian Dys Sahagun <briansahagun@gmail.com>
2017-05-10 19:22:45 +0800

    Updates

--- snapons/applicator/index.php
+++ snapons/applicator/index.php
@@ -16,16 +16,19 @@ if ( ! function_exists( 'apl_applicator_func_class' ) ) {
         
         // Settings
         $main_menu_setting = 'default'; // default | right
+        $main_search_setting = 'left'; // default | left
         
         
         echo ' ' . $snapon_name;
         echo ' ' . $snapon_name . '--' . 'go-content-nav';
         echo ' ' . $snapon_name . '--' . 'go-start-nav';
         
-        echo ' ' . $snapon_name . '--' . 'main-menu';
-        echo ' ' . $snapon_name . '--' . 'main-menu' . '--' . $main_menu_setting; 
+        // echo ' ' . $snapon_name . '--' . 'main-menu';
+        echo ' ' . $snapon_name . '--' . 'main-menu' . '--' . $main_menu_setting;
         
         echo ' ' . $snapon_name . '--' . 'main-search';
+        echo ' ' . $snapon_name . '--' . 'main-search' . '--' . $main_search_setting;
+        
         echo ' ' . $snapon_name . '--' . 'sub-nav';
     
     }
@@ -61,7 +64,7 @@ if ( ! function_exists( 'apl_snapons_applicator_func_styles' ) ) {
         wp_enqueue_style( 'apl-style-fonts', applicator_fonts_url(), array(), null );
         add_editor_style( array( 'assets/css/editor-style.css', applicator_fonts_url() ) );
         
-        wp_enqueue_style( 'apl-snapons-applicator-style', get_theme_file_uri() . '/snapons/applicator/assets/applicator.css', array(), '18.5', 'all' );
+        wp_enqueue_style( 'apl-snapons-applicator-style', get_theme_file_uri() . '/snapons/applicator/assets/applicator.css', array(), '18.7', 'all' );
 
     }
     add_action( 'wp_enqueue_scripts', 'apl_snapons_applicator_func_styles', 0);
@@ -72,7 +75,7 @@ if ( ! function_exists( 'apl_snapons_applicator_func_styles' ) ) {
 if ( ! function_exists( 'apl_snapons_applicator_scripts' ) ) {
     function apl_snapons_applicator_scripts() {
         
-        wp_enqueue_script( 'apl-snapons-applicator-script-global', get_theme_file_uri( '/snapons/applicator/assets/applicator.js' ), array( 'jquery' ), '17.3', true );
+        wp_enqueue_script( 'apl-snapons-applicator-script-global', get_theme_file_uri( '/snapons/applicator/assets/applicator.js' ), array( 'jquery' ), '17.4', true );
         
         // EQCSS
         // wp_enqueue_script( 'apl-snapons-applicator-script-eqcss', get_theme_file_uri( '/snapons/applicator/assets/eqcss.min.js' ), array(), '1.0', true );
