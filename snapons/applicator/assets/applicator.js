e7557bdc56d25e1a6e4de905edf480ea84608360
Brian Dys Sahagun <briansahagun@gmail.com>
2017-07-23 23:43:46 +0800

    Updates

--- snapons/applicator/assets/applicator.js
+++ snapons/applicator/assets/applicator.js
@@ -892,58 +892,71 @@
     
     } )();
     
+    
+    
+    
+    
+    // ------------------------- Form Validation
+    ( function() {
+    
     $commentFormNote = aplDataCommentFormNote.commentFormNote;
-    
-    
-    var forms = $( '#commentform' );
-    for ( var i = 0; i < forms.length; i++ ) {
-        forms[i].noValidate = true;
+        
+        var forms = $( '#commentform' );
+        
+        for ( var i = 0; i < forms.length; i++ ) {
+            forms[i].noValidate = true;
 
-        forms[i].addEventListener( 'submit', function( event ) {
-            
-            //Prevent submission if checkValidity on the form returns false.
-            if ( ! event.target.checkValidity() ) {
-                event.preventDefault();
-                
-                $('#commentform :input:visible[required="required"]').each( function () {
-                    
-                    $( this ).closest( '.cr' ).find( '.validity-note' ).remove();
-                    
-                    if ( ! this.validity.valid ) {
+            forms[i].addEventListener( 'submit', function( event ) {
+
+                //Prevent submission if checkValidity on the form returns false.
+                if ( ! event.target.checkValidity() ) {
+                    event.preventDefault();
+
+                    $('#commentform :input:visible[required="required"]').each( function () {
                         
-                        validityNoteContainerGenericElementLabel = $( '<div />', {
-                            'class': 'g_l validty-note---g_l',
-                            'html': '<p>' + this.validationMessage + '</p>'
-                        } );
-                        
-                        validityNoteContainerGenericElement = $( '<div />', {
-                            'class': 'g validty-note---g'
-                        } ).append( validityNoteContainerGenericElementLabel );
-                        
-                        validityNoteContainer = $( '<div />', {
-                            'class': 'cr validty-note---cr'
-                        } ).append( validityNoteContainerGenericElement );
-                        
-                        validityNote = $( '<div />', {
-                            'class': 'obj note validity-note',
-                            'data-name': 'Validity Note OBJ'
-                        } ).append( validityNoteContainer );
-                      
-                    $( this ).focus();
-                    
-                    $( this ).closest( '.cp' )
-                        .addClass( 'creation--invalid' )
-                        .find( '.cr' )
-                        .append( validityNote );
-                    
-                    return false;
-                    }
-                } );
-                
-                return;
-            }
-        }, false);
-    }
+                        var _this = $( this );
+
+                        _this.closest( '.cr' ).find( '.validity-note' ).remove();
+
+                        if ( ! this.validity.valid ) {
+
+                            var validityNoteTerm = 'validity-note';
+
+                            validityNoteContainerGenericElementLabel = $( '<div />', {
+                                'class': 'g_l '+ validityNoteTerm +'---g_l',
+                                'html': '<p>' + this.validationMessage + '</p>'
+                            } );
+
+                            validityNoteContainerGenericElement = $( '<div />', {
+                                'class': 'g '+ validityNoteTerm +'---g'
+                            } ).append( validityNoteContainerGenericElementLabel );
+
+                            validityNoteContainer = $( '<div />', {
+                                'class': 'cr '+ validityNoteTerm +'---cr'
+                            } ).append( validityNoteContainerGenericElement );
+
+                            validityNote = $( '<div />', {
+                                'class': 'obj note '+ validityNoteTerm +'',
+                                'data-name': 'Validity Note OBJ'
+                            } ).append( validityNoteContainer );
+
+                        _this.focus();
+
+                        _this.closest( '.cp' )
+                            .addClass( 'creation--invalid' )
+                            .find( '.cr' )
+                            .append( validityNote );
+
+                        return false;
+                        }
+                    } );
+
+                    return;
+                }
+            }, false);
+        }
+    
+    } )();
     
     /*
     var actions = {
