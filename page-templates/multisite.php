b29a436d215bc144e1dc1ce6c44c2178dca69fb3
Brian Dys Sahagun <briansahagun@gmail.com>
2017-08-20 20:20:51 +0800

    Updates

--- page-templates/multisite.php
+++ page-templates/multisite.php
@@ -59,7 +59,7 @@ else {
 
                 if ( $the_query->have_posts() ) {
                     
-                    // OB: Entries Content
+                    // OB: Sites Content
                     ob_start();
                     while ( $the_query->have_posts() ) {
                         $the_query->the_post();
@@ -75,101 +75,12 @@ else {
                         'structure' => array(
                             'type'      => 'component',
                         ),
-                        'hr_content'    => applicator_func_page_nav(),
                         'content'   => array(
                             'component'     => array(
                                 $entries_ob_content,
                             ),
                         ),
-                        'fr_content'    => applicator_func_page_nav(),
                     ) );
-
-                    
-                    /*
-                    while ( $the_query->have_posts() ) {
-                        $the_query->the_post();
-                        
-                        ?>
-                        
-                        <article <?php post_class( 'cp article' ); ?> data-name="Post CP">
-                            <div class="cr post---cr">
-                                <header class="hr post---hr entry-header">
-                                    <div class="hr_cr post---hr_cr">
-        
-                                        <?php
-
-                                        // E: Main Post Title
-                                        $post_title_obj = applicator_htmlok( array(
-                                            'name'      => 'Main Post Title',
-                                            'structure' => array(
-                                                'type'      => 'object',
-                                                'elem'      => 'h1',
-                                                'linked'    => true,
-                                                'layout'    => 'inline',
-                                                'attr'      => array(
-                                                    'a'         => array(
-                                                        'href'      => esc_url( get_permalink() ),
-                                                        'rel'       => 'bookmark',
-                                                        'title'     => get_the_title(),
-                                                    ),
-                                                ),
-                                            ),
-                                            'content'   => array(
-                                                'object'        => get_the_title(),
-                                            ),
-                                            'echo'      => true,
-                                        ) );
-
-                                        ?>
-                                        
-                                    </div>
-                                </header>
-                                
-                                <?php
-                                if ( has_excerpt() ) {
-                                ?>
-                                
-                                <div class="ct post---ct entry-content">
-                                    <div class="ct_cr post---ct_cr">
-                                        
-                                        <?php
-                                        // OB: Excerpt
-                                        ob_start();
-                                        the_excerpt();
-                                        $excerpt_ob_content = ob_get_contents();
-                                        ob_end_clean();
-
-
-                                        // R: Post Excerpt
-                                        $post_excerpt = applicator_htmlok( array(
-                                            'name'      => 'Post Excerpt',
-                                            'structure' => array(
-                                                'type'      => 'component',
-                                            ),
-                                            'content'   => array(
-                                                'component'     => $excerpt_ob_content,
-                                            ),
-                                        ) );
-
-                                        // E: Post Excerpt
-                                        echo $post_excerpt;
-                                        ?>
-                                        
-                                    </div>
-                                </div>
-                                
-                                <?php
-                                }
-                                ?>
-                            
-                            </div>
-                        </article><!-- Post CP -->
-                        
-                        
-                        
-                    <?php
-                    }
-                    */
                     
                 }
                 wp_reset_postdata();
@@ -183,22 +94,6 @@ else {
                     ),
                     'content'   => array(
                         'component'     => $entry_entries_cp,
-                    ),
-                ) );
-
-
-                // Primary Content
-                $primary_content = applicator_htmlok( array(
-                    'name'      => 'Primary Content',
-                    'structure' => array(
-                        'type'      => 'constructor',
-                        'elem'      => 'main',
-                    ),
-                    'id'        => 'main',
-                    'css'       => 'pri-content',
-                    'root_css'  => 'site-main',
-                    'content'   => array(
-                        'constructor'   => $entry_module_cp,
                     ),
                     'echo'      => true,
                 ) );
