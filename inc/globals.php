11686c1b3ea7bb199432cbf928624e19937c1671
Brian Dys Sahagun <briansahagun@gmail.com>
2017-09-03 15:00:20 +0800

    Updates

--- inc/globals.php
+++ inc/globals.php
@@ -1,5 +1,6 @@
 <?php // Global Variables
 $GLOBALS['colon_sep'] = '<span class="sep colon---sep">:</span>';
 $GLOBALS['comma_sep'] = '<span class="sep comma---sep">, </span>';
-$GLOBALS['space_sep'] = '<span class="sep space---sep"> </span>';
+// $GLOBALS['space_sep'] = '<span class="sep space---sep"> </span>';
+$GLOBALS['space_sep'] = ' ';
 $GLOBALS['ellipsis_sep'] = '<span class="sep ellipsis---sep">&hellip;</span>';
