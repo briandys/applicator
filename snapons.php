3428be10e5373ec64c881b471e49bf8e84b337b0
Brian Dys Sahagun <briansahagun@gmail.com>
2017-09-22 01:08:53 +0800

    Updates

--- snapons.php
+++ snapons.php
@@ -2,3 +2,6 @@
 
 // Applicator
 require_once get_template_directory() . '/snapons/applicator/index.php';
+
+// Multisite Directory
+require_once get_template_directory() . '/snapons/multisite-directory/index.php';
